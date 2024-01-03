925736e3fbb695c2839c342edc73343f80af0334
phamtiengoco <155243756+phamtiengoco@users.noreply.github.com>
2024-01-03 22:38:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,18 @@ if (cluster.isPrimary) {
       io.emit('chat message', msg, result.lastID);
       
     });
+        if (!socket.recovered) {
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+      }
+    }
     socket.on("disconnect", (reason) => {
      console.log( socket.client.conn.server.clientsCount + " Còn lại " );
     });
